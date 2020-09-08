e4e4519a939ebd6578d5d79c672d3bd7d8b596b2
AntonioNtV <antoniontv18@gmail.com>
2020-09-08 12:16:13 -0300

    Inital commit

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/', (request, response) => {
+    response.json({
+        message: 'Hello World! 🌎',
+    });
+});
+
+export default routes;
