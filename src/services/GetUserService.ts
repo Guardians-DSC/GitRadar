d3bf222bf003223b7dba95c34f56b3046853c376
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-10 08:50:16 -0300

    getUserService created

---
+++ src/services/GetUserService.ts
@@ -0,0 +1,20 @@
+import api from './api';
+
+interface ProfileInfo {
+  github_login: string;
+  avatar_url: string;
+}
+
+class GetUserService {
+  public async execute(username: string): Promise<ProfileInfo> {
+    const response = await api.get(`/users/${username}`);
+    const { login, avatar_url } = response.data;
+
+    return {
+      github_login: login,
+      avatar_url,
+    };
+  }
+}
+
+export default GetUserService;
