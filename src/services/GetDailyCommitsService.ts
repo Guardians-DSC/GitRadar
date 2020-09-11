6ab26d813b434c6d9e186e30c773e57ae17de895
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-11 16:06:53 -0300

    DailyCommitsService Reponse added

--- src/services/GetDailyCommitsService.ts
+++ src/services/GetDailyCommitsService.ts
@@ -1,6 +1,19 @@
 import api from './api';
 import Event from '../entities/Event';
 
+interface Commit {
+  repository: {
+    name: string;
+    html_url: string;
+  };
+  message: string;
+}
+
+interface Response {
+  new_commits: number;
+  commits: Commit[];
+}
+
 class GetDailyCommitsService {
   public async execute(username: string) {
     const response = await api.get(`/users/${username}/events`);
