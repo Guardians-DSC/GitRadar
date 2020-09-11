43df02ca36d3a208b1090dce4e58a8b4297132bb
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-11 13:26:16 -0300

    DailyEventsService's Event interface updated

--- src/services/GetDailyEventsService.ts
+++ src/services/GetDailyEventsService.ts
@@ -1,8 +1,24 @@
 import api from './api';
 
+interface Commit {
+  message: string;
+}
+
+interface Event {
+  type: string;
+  repo: {
+    name: string;
+    url: string;
+  };
+  payload: {
+    commits: Commit[];
+  };
+}
+
 class GetDailyEventsService {
   public async execute(username: string) {
     const response = await api.get(`/users/${username}/events`);
+    const events: Event[] = response.data;
   }
 }
 
