209940fe10bed6db02db8b5f8739e942802674fc
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-11 17:10:51 -0300

    server.ts changed back to normal

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express';
 import routes from './routes';
 
-import GetUserDailyService from './services/GetUserDailyService';
+const app = express();
+app.use(express.json());
+app.use(routes);
 
-(async () => {
-  const response = await new GetUserDailyService().execute('davigsousa');
-  console.log(response);
-})();
-
-// const app = express();
-// app.use(express.json());
-// app.use(routes);
-
-// app.listen(3333, () => {
-//   console.log('🚀 Server started on port 3333');
-// });
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333');
+});
