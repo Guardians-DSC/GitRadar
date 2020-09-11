6fc4719aca8e722e8ac9ae284d5e747a98f63485
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-11 16:46:22 -0300

    DailyCommitsService added

--- src/services/GetDailyCommitsService.ts
+++ src/services/GetDailyCommitsService.ts
@@ -4,7 +4,7 @@ import Event from '../entities/Event';
 interface Commit {
   repository: {
     name: string;
-    html_url: string;
+    url: string;
   };
   message: string;
 }
@@ -14,10 +14,44 @@ interface Response {
   commits: Commit[];
 }
 
+function isToday(a: Date, b: Date) {
+  return (
+    a.getDate() === b.getDate() &&
+    a.getMonth() === b.getMonth() &&
+    a.getFullYear() === b.getFullYear()
+  );
+}
+
 class GetDailyCommitsService {
   public async execute(username: string) {
     const response = await api.get(`/users/${username}/events`);
-    const events: Event[] = response.data;
+    let events: Event[] = response.data;
+
+    events = events.filter(item => {
+      const eventDate = new Date(item.created_at);
+      const today = new Date();
+
+      return isToday(today, eventDate) && item.payload.commits;
+    });
+
+    let commits: Commit[] = [];
+    events.forEach(item => {
+      if (item.payload.commits) {
+        const newCommits: Commit[] = item.payload.commits.map(commit => {
+          return {
+            repository: item.repo,
+            message: commit.message,
+          };
+        });
+
+        commits = [...commits, ...newCommits];
+      }
+    });
+
+    return {
+      new_commits: commits.length,
+      commits,
+    };
   }
 }
 
