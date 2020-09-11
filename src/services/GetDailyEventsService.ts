056ffc899cd1ba0400ac3d795866f3668c982b47
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-11 14:09:51 -0300

    Event entity created

--- src/services/GetDailyEventsService.ts
+++ src/services/GetDailyEventsService.ts
@@ -13,12 +13,58 @@ interface Event {
   payload: {
     commits: Commit[];
   };
+  created_at: string;
+}
+
+interface EventsCounter {
+  [type: string]: number;
+}
+
+interface Response {
+  new_interactions: number;
+  new_forks: number;
+  new_stars: number;
+  new_commits: number;
+  new_repositories: number;
+  new_prs: number;
+  new_issues: number;
 }
 
 class GetDailyEventsService {
-  public async execute(username: string) {
+  public async execute(username: string): Promise<Response> {
     const response = await api.get(`/users/${username}/events`);
-    const events: Event[] = response.data;
+    let events: Event[] = response.data;
+
+    events = events.filter(item => {
+      const eventDate = new Date(item.created_at);
+      const today = new Date();
+
+      const isToday =
+        eventDate.getDate() === today.getDate() &&
+        eventDate.getMonth() === today.getMonth() &&
+        eventDate.getFullYear() === today.getFullYear();
+
+      return isToday;
+    });
+
+    const eventsCounter: EventsCounter = {};
+    events.forEach(item => {
+      if (eventsCounter[item.type]) {
+        eventsCounter[item.type] += 1;
+      } else {
+        eventsCounter[item.type] = 1;
+      }
+    });
+
+    return {
+      new_forks: eventsCounter['ForkEvent'],
+      new_issues: eventsCounter['IssueEvent'],
+      new_prs: eventsCounter['PullRequestEvent'],
+      new_stars: eventsCounter['WatchEvent'],
+      new_repositories: 0,
+      new_commits: 0,
+      new_interactions: 0,
+    };
   }
 }
 
