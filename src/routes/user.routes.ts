e0b399ed54ecaa8aa6059b9a78656865cc71fabc
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-11 18:41:18 -0300

    user daily route created

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 
 import GetProfileService from '../services/GetProfileService';
 import GetLanguagesService from '../services/GetLanguagesService';
+import GetUserDailyService from '../services/GetUserDailyService';
 
 const userRouter = Router();
 
@@ -25,6 +26,21 @@ userRouter.get('/', async (request: Request, response: Response) => {
   });
 });
 
-userRouter.get('/daily', async (request: Request, response: Response) => {});
+userRouter.get('/daily', async (request: Request, response: Response) => {
+  const { username } = request.body;
+
+  if (!username)
+    return response
+      .status(400)
+      .json({ error: 'GitHub username does not provided.' });
+
+  const getUserDailyService = new GetUserDailyService();
+
+  const dailyInfo = await getUserDailyService.execute(username);
+
+  return response.json({
+    ...dailyInfo,
+  });
+});
 
 export default userRouter;
