42f99e3dad6b43c5ee27041c9058fde7c1ae3150
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-15 22:07:08 -0300

    app error handler middleware added

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,24 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+      if (error instanceof AppError) {
+          return response.status(error.statusCode).json({
+              status: 'error',
+              message: error.message,
+          });
+      }
+
+      console.error(error);
+
+      return response.status(500).json({
+          status: 'error',
+          message: 'Internal server error',
+      });
+  },
+);
+
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
 });
