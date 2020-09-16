9a1a62abfa093ce96c576d7ab34421fc6a0f7b5a
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-16 08:57:15 -0300

    errorHandler Middleware moved to its folder

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,13 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import 'express-async-errors';
 import routes from './routes';
-import AppError from './errors/AppError';
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use(
-  (error: Error, request: Request, response: Response, _: NextFunction) => {
-      if (error instanceof AppError) {
-          return response.status(error.statusCode).json({
-              status: 'error',
-              message: error.message,
-          });
-      }
-
-      console.error(error);
-
-      return response.status(500).json({
-          status: 'error',
-          message: 'Internal server error',
-      });
-  },
-);
+app.use(errorHandlerMiddleware);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
