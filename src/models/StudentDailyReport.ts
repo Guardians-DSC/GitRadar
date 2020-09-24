bf143b08e21be15517b8e7e7caa2a4bbb3d02164
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-24 19:29:19 -0300

    StudentDailyReport model added

---
+++ src/models/StudentDailyReport.ts
@@ -0,0 +1,59 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Student from './Student';
+
+@Entity('student_daily_reports')
+class Repository {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('uuid')
+  student_id: string;
+
+  @ManyToOne(() => Student)
+  @JoinColumn({ name: 'student_id' })
+  student: Student;
+
+  @Column('int2')
+  new_forks: number;
+
+  @Column('int2')
+  new_issues: number;
+
+  @Column('int2')
+  new_prs: number;
+
+  @Column('int2')
+  new_stars: number;
+
+  @Column('int2')
+  new_repositories: number;
+
+  @Column('int2')
+  new_interactions: number;
+
+  @Column('int2')
+  new_commits: number;
+
+  @Column('int2')
+  additions: number;
+
+  @Column('int2')
+  deletions: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Repository;
