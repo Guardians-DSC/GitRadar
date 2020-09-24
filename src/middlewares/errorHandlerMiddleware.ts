5a0e1394561838e5d874f67bb74ac2c5eef9a69a
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-23 21:53:49 -0300

    eslint errors fixed

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,20 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
 import AppError from '../errors/AppError';
 
-function errorHandlerMiddleware(error: Error, request: Request, response: Response, _: NextFunction) {
+function errorHandlerMiddleware(
+  error: Error,
+  request: Request,
+  response: Response,
+  _: NextFunction,
+): Response {
   if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-          status: 'error',
-          message: error.message,
-      });
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
   }
 
   console.error(error);
 
   return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
+    status: 'error',
+    message: 'Internal server error',
   });
-};
+}
 
 export default errorHandlerMiddleware;
