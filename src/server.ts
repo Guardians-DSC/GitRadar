da63ddaa17c48ee829549594bdc5abe35f811745
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-09-25 00:10:37 -0300

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import 'reflect-metadata';
 import dotenv from 'dotenv';
@@ -10,6 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use(errorHandlerMiddleware);
