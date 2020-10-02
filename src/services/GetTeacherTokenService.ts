34de5261da6aa0880f8fad7ed6b892f42cbe6ab2
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-10-01 22:51:25 -0300

    decrypt function added on GetTeacherTokenService

--- src/services/GetTeacherTokenService.ts
+++ src/services/GetTeacherTokenService.ts
@@ -1,5 +1,22 @@
 import { getRepository } from 'typeorm';
+import crypto from 'crypto';
+import AppError from '../errors/AppError';
 import Teacher from '../models/Teacher';
+
+function decrypt(word: string): string {
+  const cryptoAlgorithm = process.env.CRYPTO_ALGORITHM;
+  const cryptoSecret = process.env.CRYPTO_SECRET;
+
+  if (!cryptoAlgorithm || !cryptoSecret) {
+    throw new AppError('Internal server error.', 500);
+  }
+
+  const iv = Buffer.alloc(16, 0);
+  const cipher = crypto.createDecipheriv(cryptoAlgorithm, cryptoSecret, iv);
+
+  cipher.update(word, 'hex', 'utf8');
+  return cipher.final('utf8');
+}
 
 class GetTeacherTokenService {
   public async execute(): Promise<string> {
@@ -9,7 +26,7 @@ class GetTeacherTokenService {
 
     const { github_token } = teacher;
 
-    return github_token;
+    return decrypt(github_token);
   }
 }
 
