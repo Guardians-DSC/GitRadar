1c3e42f81cf3dbc4250c1a109e7e7389d621dcb3
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-10-02 17:37:54 -0300

    global teacher token defined

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,24 @@
 import axios from 'axios';
 import GetTeacherTokenService from './GetTeacherTokenService';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const useGlobal: any = global;
+
 const api = axios.create({
   baseURL: 'https://api.github.com',
 });
 
 api.interceptors.request.use(async config => {
-  const getTeacherToken = new GetTeacherTokenService();
-  const token = await getTeacherToken.execute();
+  if (!useGlobal.teacherToken) {
+    const getTeacherToken = new GetTeacherTokenService();
+    const token = await getTeacherToken.execute();
 
-  if (token) {
-    config.headers.authorization = `token ${token}`;
+    if (token) {
+      useGlobal.teacherToken = token;
+    }
+  }
+  if (useGlobal.teacherToken) {
+    config.headers.authorization = `token ${useGlobal.teacherToken}`;
   }
 
   return config;
