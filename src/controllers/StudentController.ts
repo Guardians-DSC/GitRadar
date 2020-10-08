a0bd0585b1c4764fb8d9c6a637a77ad8d811b519
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-10-08 15:24:11 -0300

    creation of repositories on StudentController added

--- src/controllers/StudentController.ts
+++ src/controllers/StudentController.ts
@@ -5,6 +5,7 @@ import GetUserService from '../services/GetUserService';
 import GetProfileService from '../services/GetProfileService';
 import GetLanguagesService from '../services/GetLanguagesService';
 import CreateStudentService from '../services/CreateStudentService';
+import CreateRepositoryService from '../services/CreateRepositoryService';
 
 class StudentController {
   static async store(request: Request, response: Response): Promise<Response> {
@@ -18,7 +19,7 @@ class StudentController {
     const { teacher } = request;
 
     const getProfile = new GetProfileService();
-    const { avatar_url } = await getProfile.execute(github_login);
+    const { avatar_url, repositories } = await getProfile.execute(github_login);
 
     const getLanguages = new GetLanguagesService();
     const { top_language } = await getLanguages.execute(github_login);
@@ -34,6 +35,19 @@ class StudentController {
       teacher_id: teacher.id,
       top_language,
     });
+
+    const createRepository = new CreateRepositoryService();
+    for (const repository of repositories) {
+      // eslint-disable-next-line no-await-in-loop
+      await createRepository.execute({
+        description: repository.description,
+        full_name: repository.full_name,
+        github_id: repository.id,
+        html_url: repository.html_url,
+        name: repository.name,
+        student_id: student.id,
+      });
+    }
 
     return response.json(student);
   }
