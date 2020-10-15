74e80309747d70e078a25c2e973560f7e70b2b0e
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-10-15 20:53:22 -0300

    SignUp component added

---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignUp: React.FC = () => {
+  return <h1>SignUp</h1>;
+};
+
+export default SignUp;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
 import Route from './Route';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
+    <Route exact path="/cadastrar" component={SignUp} />
   </Switch>
 );
 
