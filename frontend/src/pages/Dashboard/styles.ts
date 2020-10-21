2a9bd03f2985af03d9dca43b281eb07348f39079
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-10-21 18:04:37 -0300

    dashboard page containers style improved

--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 export const PageContainer = styled.div`
   width: 100vw;
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
 export const Container = styled.div`
-  width: 900px;
+  width: 1000px;
   height: 100%;
   padding: 50px;
   display: flex;
