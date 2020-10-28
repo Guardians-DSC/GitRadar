8dc7a4e407a55e8a268cf195ecf5633664d0910d
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-10-28 17:32:35 -0300

    Repository Controller added and repositories route added

--- backend/src/routes/student.routes.ts
+++ backend/src/routes/student.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import StudentController from '../controllers/StudentController';
+import RepositoryController from '../controllers/RepositoryController';
 
 const studentRouter = Router();
 
 studentRouter.post('/', StudentController.store);
-studentRouter.get('/report/:username', StudentController.show);
+studentRouter.get('/:username/report', StudentController.show);
+studentRouter.get('/:username/repositories', RepositoryController.index);
 
 export default studentRouter;
