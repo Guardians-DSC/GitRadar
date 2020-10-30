df630e5ed7c824559280ef4dab0f55ae83e7c43b
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-10-30 11:41:47 -0300

    RepositoryController removed and DailyReportController created

--- backend/src/routes/student.routes.ts
+++ backend/src/routes/student.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
 import StudentController from '../controllers/StudentController';
-import RepositoryController from '../controllers/RepositoryController';
+import DailyReportController from '../controllers/DailyReportController';
 
 const studentRouter = Router();
 
 studentRouter.post('/', StudentController.store);
-studentRouter.get('/:username/report', StudentController.show);
-studentRouter.get('/:username/repositories', RepositoryController.index);
+studentRouter.get('/:username', StudentController.show);
+studentRouter.get('/:username/report', DailyReportController.show);
 
 export default studentRouter;
