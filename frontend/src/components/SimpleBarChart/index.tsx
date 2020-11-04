6a4f8499858e716b021d0544e37204c2f21237eb
AntonioNtV <antoniontv18@gmail.com>
2020-11-04 14:05:58 -0300

    SimpleBarChart component added

---
+++ frontend/src/components/SimpleBarChart/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import { Container, Title } from './styles'
+
+interface SimpleBarChartProps {
+    title: string;
+    data: object[];
+    bars: BarProps[]
+    xAxisName: string
+}
+
+interface BarProps {
+    name: string
+    dataKey: string
+    fill: string
+    stackId: string
+  }
+  
+
+const SimpleBarChar: React.FC<SimpleBarChartProps> = ({ title, data, bars, xAxisName }: SimpleBarChartProps) => (
+    <Container>
+        <Title>{title}</Title>
+        <ResponsiveContainer height="100%" width="100%">
+            <BarChart
+                data={data}
+            >
+                <CartesianGrid strokeDasharray="3 3" stroke="#eee"/>
+                <XAxis dataKey={xAxisName} />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                {
+                bars.map((bar) => (
+                    <Bar 
+                    key={bar.name}
+                    name={bar.name}
+                    dataKey={bar.dataKey}
+                    fill={bar.fill}
+                    />
+            ))
+          }
+            </BarChart>
+        </ResponsiveContainer>
+    </Container>
+)
+
+export default SimpleBarChar
