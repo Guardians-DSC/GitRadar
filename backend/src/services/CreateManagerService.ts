1b54c025fe607e1466185a598c4a33a15b0fe7cb
AntonioNtV <antoniontv18@gmail.com>
2020-11-18 13:13:19 -0300

    Creating manager ok

--- backend/src/services/CreateManagerService.ts
+++ backend/src/services/CreateManagerService.ts
@@ -1,6 +1,8 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import api from './githubApi/RestApi';
 import Manager from '../models/Manager';
+import AppError from '../errors/AppError';
 
 interface Request {
   github_login: string;
@@ -22,13 +24,35 @@ class CreateManagerService {
     email,
     password,
   }: Request): Promise<Response> {
-    const managerRepository = getRepository(Manager);
+    const managersRepository = getRepository(Manager);
+
+    const existsManager = await managersRepository.findOne({
+      email,
+    });
+
+    if (existsManager) {
+      throw new AppError('E-mail address already used');
+    }
 
     const githubData = await api.get(`/users/${github_login}`);
 
     const { login, name, avatar_url, id } = githubData.data;
 
-    return null;
+    const hashedPassword = await hash(password, 8);
+
+    const manager = await managersRepository.create({
+      avatar_url,
+      email,
+      name,
+      github_login: login,
+      github_id: id,
+      password: hashedPassword,
+    });
+
+    await managersRepository.save(manager);
+    delete manager.password;
+
+    return manager;
   }
 }
 
