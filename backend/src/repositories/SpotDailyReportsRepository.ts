9fb956ca73b74e35478942f1ce826194fbe03515
AntonioNtV <antoniontv18@gmail.com>
2020-11-19 10:30:12 -0300

    adding fields into report spot

--- backend/src/repositories/SpotDailyReportsRepository.ts
+++ backend/src/repositories/SpotDailyReportsRepository.ts
@@ -25,7 +25,13 @@ interface Report {
   spot: {
     id: string;
     github_login: string;
+    manager_id: string;
+    avatar_url: string;
+    top_language: string;
+    github_id: string;
     name: string;
+    created_at: string;
+    updated_at: string;
   };
   metrics: {
     new_interactions: number;
@@ -108,6 +114,12 @@ class SpotDailyReportsRepository extends Repository<SpotDailyReport> {
       spot_github_login,
       spot_id,
       spot_name,
+      spot_avatar_url,
+      spot_top_language,
+      spot_created_at,
+      spot_github_id,
+      spot_manager_id,
+      spot_updated_at,
     } = rawReport;
 
     const report: Report = {
@@ -115,6 +127,12 @@ class SpotDailyReportsRepository extends Repository<SpotDailyReport> {
         id: spot_id,
         github_login: spot_github_login,
         name: spot_name,
+        avatar_url: spot_avatar_url,
+        top_language: spot_top_language,
+        github_id: spot_github_id,
+        manager_id: spot_manager_id,
+        created_at: spot_created_at,
+        updated_at: spot_updated_at,
       },
       metrics: {
         additions: Number(additions),
