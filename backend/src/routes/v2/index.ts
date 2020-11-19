5055118e5e1cc53dda0130e83f3ef6eb5a9499c6
AntonioNtV <antoniontv18@gmail.com>
2020-11-19 10:06:06 -0300

    Removing test route

--- backend/src/routes/v2/index.ts
+++ backend/src/routes/v2/index.ts
@@ -10,22 +10,5 @@ const routes = Router();
 routes.use('/manager', manangerRouter);
 routes.use('/spot', spotRouter);
 routes.use('/session', sessionsRouter);
-routes.get('/test', async (request, response) => {
-  const spotDailyReportsRepository = getCustomRepository(
-    SpotDailyReportsRepository,
-  );
-
-  const since = new Date();
-
-  since.setMonth(since.getMonth() - 1);
-
-  const data = await spotDailyReportsRepository.findByPeriod(
-    'af733d6f-c198-41ac-9299-f6524a7108d9',
-    since,
-    new Date(),
-  );
-
-  return response.json(data);
-});
 
 export default routes;
