b0420555eaa2b4006d429f850aec252afe99d4cf
AntonioNtV <antoniontv18@gmail.com>
2020-11-21 10:02:12 -0300

    Using github_login to get spot data on routes

--- backend/src/services/Spot/GetSpotRepositories.ts
+++ backend/src/services/Spot/GetSpotRepositories.ts
@@ -1,17 +1,30 @@
 import { getRepository } from 'typeorm';
 import Repository from '../../models/Repository';
+import Spot from '../../models/Spot';
+import { AppError } from '../../errors/AppError';
 
 interface Request {
-  spot_id: string;
+  github_login: string;
 }
 
 class GetSpotRepositories {
-  async execute({ spot_id }: Request): Promise<Repository[]> {
+  async execute({ github_login }: Request): Promise<Repository[]> {
     const repositoriesRepository = getRepository(Repository);
+    const spotRepository = getRepository(Spot);
+
+    const spot = await spotRepository.findOne({
+      where: {
+        github_login,
+      },
+    });
+
+    if (!spot) {
+      throw new AppError('Spot not registered', 400);
+    }
 
     const repositories = await repositoriesRepository.find({
       where: {
-        spot_id,
+        spot_id: spot.id,
       },
     });
 
