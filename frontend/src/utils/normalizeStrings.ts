2ee375d49f89837c20bf8d20466720d1ead6604b
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-11-28 22:45:14 -0300

    hotfix: + 1 in month value added to normalizeDateLabel

--- frontend/src/utils/normalizeStrings.ts
+++ frontend/src/utils/normalizeStrings.ts
@@ -10,9 +10,10 @@ export function normalizeName(original: string, limit: number): string {
 export function normalizeInformation(original: number): string {
   return String(original.toFixed(2));
 }
+
 export function normalizeDateLabel(original: Date): string {
   const day = String(original.getDate()).padStart(2, '0');
-  const month = String(original.getMonth()).padStart(2, '0');
+  const month = String(original.getMonth() + 1).padStart(2, '0');
 
   return `${day}/${month}`;
 }
